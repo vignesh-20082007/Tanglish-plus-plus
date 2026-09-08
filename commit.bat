88cadac560ce16ff9836e8c53cebf38fd54517c5
Arumugam <arumugam@example.com>
2026-09-08 23:24:03 +0530

    chore: clean up repo

--- commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "PATH=C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd;%PATH%"
-git config user.name Arumugam
-git config user.email arumugam@example.com
-git add -A
-git commit -m "Initial commit: Tanglish++ Web IDE"
-git log -n 1
